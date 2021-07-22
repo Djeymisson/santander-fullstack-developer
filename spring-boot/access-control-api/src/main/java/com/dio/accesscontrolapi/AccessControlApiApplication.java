ab414ccfc64c9b0ec51acdd9498a93edb842d0a0
djeymisson <djeymisson@gmail.com>
2021-07-22 16:04:01 -0300

    creating spring boot project (sistema de controle de ponto e acesso)

---
+++ spring-boot/access-control-api/src/main/java/com/dio/accesscontrolapi/AccessControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.accesscontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessControlApiApplication.class, args);
+	}
+
+}
---
+++ spring-boot/access-control-api/src/test/java/com/dio/accesscontrolapi/AccessControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.accesscontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
