f5a798ab586b2dce00a879d41226b5ed5b5cbd7e
djeymisson <djeymisson@gmail.com>
2021-08-10 11:57:25 -0300

    refactoring controller

--- spring-cloud/shopping-cart/src/main/java/com/digitalinnovation/one/shoppingcart/controller/CartController.java
+++ spring-cloud/shopping-cart/src/main/java/com/digitalinnovation/one/shoppingcart/controller/CartController.java
@@ -4,9 +4,9 @@ import com.digitalinnovation.one.shoppingcart.model.Cart;
 import com.digitalinnovation.one.shoppingcart.model.Item;
 import com.digitalinnovation.one.shoppingcart.repository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(value = "/cart")
@@ -25,14 +25,17 @@ public class CartController {
     }
 
     @GetMapping("/{id}")
-    public Optional<Cart> findById(@PathVariable("id") Integer id) {
-        return cartRepository.findById(id);
+    public Cart findById(@PathVariable("id") Integer id) {
+        return cartRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "entity not found"
+                ));
+
     }
 
     @DeleteMapping("/{id}")
     public void clear(@PathVariable("id") Integer id) {
         cartRepository.deleteById(id);
     }
-
 
 }
--- spring-cloud/shopping-cart/src/main/java/com/digitalinnovation/one/shoppingcart/repository/CartRepository.java
+++ spring-cloud/shopping-cart/src/main/java/com/digitalinnovation/one/shoppingcart/repository/CartRepository.java
@@ -2,7 +2,9 @@ package com.digitalinnovation.one.shoppingcart.repository;
 
 import com.digitalinnovation.one.shoppingcart.model.Cart;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartRepository extends CrudRepository<Cart, Integer> {
 
     Cart findByCustomerId(Integer customerId);
