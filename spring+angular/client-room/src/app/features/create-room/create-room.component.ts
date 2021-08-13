f9f47a6709208021a7d81a43b079254d68807507
djeymisson <djeymisson@gmail.com>
2021-08-13 17:11:27 -0300

    finished crud client

--- spring+angular/client-room/src/app/features/create-room/create-room.component.ts
+++ spring+angular/client-room/src/app/features/create-room/create-room.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Room } from 'src/app/shared/models/room.model';
+import { RoomService } from 'src/app/shared/services/room.service';
 
 @Component({
   selector: 'app-create-room',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRoomComponent implements OnInit {
 
-  constructor() { }
+  room: Room = new Room();
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(private roomService: RoomService,
+    private router: Router) { }
+
+  ngOnInit() {
   }
 
+  newRoom(): void {
+    this.submitted = false;
+    this.room = new Room();
+  }
+
+  save() {
+    this.roomService.createRoom(this.room)
+      .subscribe(
+        data => console.log(data),
+        error => console.log(error)
+      );
+
+    this.room = new Room();
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
+
+  gotoList() {
+    this.router.navigate(['/rooms']);
+  }
 }
--- spring+angular/client-room/src/app/features/room-details/room-details.component.ts
+++ spring+angular/client-room/src/app/features/room-details/room-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Room } from 'src/app/shared/models/room.model';
+import { RoomService } from 'src/app/shared/services/room.service';
 
 @Component({
   selector: 'app-room-details',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  room: Room;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private router: Router,
+    private roomService: RoomService) { }
+
+  ngOnInit() {
+    this.room = new Room();
+
+    this.id = this.route.snapshot.params['id'];
+
+    this.roomService.getRoom(this.id)
+      .subscribe(data => {
+        console.log(data)
+        this.room = data;
+      }, error => console.log(error));
   }
 
+  list() {
+    this.router.navigate(['rooms']);
+  }
 }
