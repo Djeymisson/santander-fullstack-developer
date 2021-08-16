da106e534a3d6bebe955cbcc1b19d3e456f6badf
djeymisson <djeymisson@gmail.com>
2021-08-16 16:22:58 -0300

    changing clock theme

--- spring+angular/client-room/src/app/shared/components/input-fields/input-time/input-time.component.ts
+++ spring+angular/client-room/src/app/shared/components/input-fields/input-time/input-time.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AbstractControl, FormGroup } from '@angular/forms';
+import { NgxMaterialTimepickerTheme } from 'ngx-material-timepicker';
 import { ValidateFieldsService } from '../validate-fields.service';
 
 @Component({
@@ -11,6 +12,21 @@ export class InputTimeComponent {
   @Input() placeholder: string;
   @Input() formGroup: FormGroup;
   @Input() controlName: string;
+
+  darkTheme: NgxMaterialTimepickerTheme = {
+    container: {
+      bodyBackgroundColor: '#424242',
+      buttonColor: '#fff'
+    },
+    dial: {
+      dialBackgroundColor: '#555',
+    },
+    clockFace: {
+      clockFaceBackgroundColor: '#555',
+      clockHandColor: '#9fbd90',
+      clockFaceTimeInactiveColor: '#fff'
+    }
+  };
 
   constructor(public validator: ValidateFieldsService) { }
 
